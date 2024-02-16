39b509aac2e750634329cf10d393deda35f45e15
Ishan <mrishan1993@gmail.com>
2024-02-15 22:11:40 -0500

    greenhouse jobs added

--- README.md
+++ README.md
@@ -3,16 +3,17 @@
 ### Product Document 
 
 
-1. Scrape roles from web3.jobs
-2. Scrape roles from greenhouse with added links for the target companies
+1. Scrape roles from web3.jobs - done
+2. Scrape roles from greenhouse with added links for the target companies - done
 3. Scrape roles from lever with added links for the target companies
 4. Scrape roles from linkedin
-5. Add filters for product manager and senior product manager with compensation more than 140k 
-6. Add filters for location Boston, New York, Remote
-7. Add the data to database
-8. Compare the data to database from last 7 days ? To be discussed ?
-9. Send new jobs to mobile phone as push notifications
-10. Cron job to fetch jobs every 6 hours and delete jobs older than 7 days ? To be discussed
-11. Deploy service on AWS
-12. Deploy database on AWS 
-13. Write a blog about it
+5. Implement pagination - done
+6. Add filters for product manager and senior product manager with compensation more than 140k - done
+7. Add filters for location Boston, New York, Remote - Do we have to implment it ? 
+8. Add the data to database
+9. Compare the data to database from last 7 days ? To be discussed ?
+10. Send new jobs to mobile phone as push notifications
+11. Cron job to fetch jobs every 6 hours and delete jobs older than 7 days ? To be discussed
+12. Deploy service on AWS
+13. Deploy database on AWS 
+14. Write a blog about it
--- controllers/fetch_jobs.js
+++ controllers/fetch_jobs.js
@@ -1,39 +1,83 @@
 var Xray = require('x-ray')
 var _ = require("lodash")
+var async = require("async");
+
 var x = Xray()
 function fetchJobs () {
-    console.log ("inside from fetch jobs")
-    return fetchFromWeb3()
+
+    const allJobs = Promise.all([fetchFromWeb3(), fetchFromGreenhouse()]);
+    allJobs.then(values => {
+        console.log("values", _.flatten(values)); // [resolvedValue1, resolvedValue2]
+    }).catch(error => {
+        console.log(error); // rejectReason of any first rejected promise
+    });
+
+    // })
+    // return allJobs
 
     
 }
 
-function fetchFromWeb3 () {
-    console.log ("inside from web3")
-    let jobs = []
-//     x('http://google.com', 'title')(function(err, title) {
-//   console.log(title) // Google
-// })
-    x('https://web3.career/product-manager-jobs', {
-        items: x(['.table_row'])
-    })(function (err, content) {
-        _.map(content.items, function (o) {
-            // o = o.replace(/\s+/g, ' ')
-            o = o.split(/\s{2,}/)
-            var obj = {
-                role: o[1],
-                company: o[2],
-                location: o[4],
-                salary_range: o[5]
+const fetchFromWeb3 = (url) => {
+    return new Promise(function(resolve, reject) {
+        let jobs = [];
+        x("https://web3.career/product-manager-jobs", '.table_row') // Selecting elements to scrape
+        .paginate('li.next > a.page-link@href') // Pagination selector
+        .limit(30) // Limiting to 3 pages but it returns only 3 items. so changed it to 30 items. // TODO
+        ((err, content) => {
+            if (err) {
+                reject(err);
+            } else {
+                content.forEach(item => {
+                    let o = item.split(/\s{2,}/);
+                    let obj = {
+                        role: o[1],
+                        company: o[2],
+                        location: o[4],
+                        salary_range: o[5]
+                    };
+                    if ((obj.salary_range && obj.salary_range.slice(-4, -1) > 140) &&
+                        (obj.role.includes("Product Manager") || obj.role.includes("Senior Product Manager"))) {
+                        jobs.push(obj);
+                    }
+                });
+                console.log("Inside web3", jobs)
+                resolve(jobs);
             }
-            jobs.push(obj)
-        })
-        return jobs
-    })
+        });
+    });
+};
 
+async function fetchFromGreenhouse () {
+    let greenhouseLinks = ["https://boards.greenhouse.io/remotecom", "https://boards.greenhouse.io/perplexityai"]
+    return new Promise(function(resolve, reject) {
+        let jobs = [];
+        for (let i=0;i<greenhouseLinks.length;i++) {
+            x(greenhouseLinks[i], ['.opening a']) // Selecting elements to scrape
+            ((err, content) => {
+                if (err) {
+                    console.log("error ", err)
+                } else {
+                    content.forEach(item => {
+                        let obj = {
+                            role: item,
+                            company: greenhouseLinks[i].slice(29)
+                        };
+    
+                        if (obj.role.includes("Product Manager") || obj.role.includes("Senior Product Manager") || obj.role.includes("Software Engineer")) {
+                            jobs.push(obj);
+                        }
+                    });
+                    
+                }
+            });    
+        }
+        console.log("Inside greenhouse", jobs)
+        resolve(jobs);
+        
+    });
     
 }
-
 
 module.exports = {
     fetchJobs
