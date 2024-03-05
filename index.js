e7d4830ef62abe06821ae301b5049bd529a539bb
Ishan <mrishan1993@gmail.com>
2024-03-05 01:47:21 -0500

    from index.js

--- index.js
+++ index.js
@@ -2,32 +2,12 @@ import express from "express";
 import { cron } from "./controllers/cron";
 import { sendNotification } from "./controllers/send_notification";
 
-const app = express();
+async function main () {
+  console.log("starting cron")
+  await sendNotification()
+  console.log("cron ended")
+}
 
-console.log("intiatl start")
 
-// app.get('/trigger-cron', async (req, res) => {
-//   try {
-//     // Call the cron job controller function directly
-//     // await cron();
-
-//     console.log("log here")
-//     await sendNotification()
-
-//     // Respond with a success message
-//     res.status(200).send("Cron job triggered successfully.");
-//   } catch (error) {
-//     // If an error occurs, respond with an error message
-//     console.error("Error triggering cron job:", error);
-//     res.status(500).send("An error occurred while triggering the cron job.");
-//   }
-// });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("log here")
-  sendNotification()
-  console.log(`Server is running on port ${PORT}`);
-});
-
+main()
 // app.use('/cron', cron);
